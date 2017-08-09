a2e4ec9f46a65bbea42cc26b06aede8fd64083b5
patmolera <pat_molera@yahoo.com>
2017-08-09 19:35:12 +0800

    New Commit - validation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,17 +7,23 @@
     <hr>
 
     <form method="POST" action="/posts">
+      {{ csrf_field() }}
       <div class="form-group">
-        <label for="title">Title</label>
-        <input type="email" class="form-control" id="title" name="title">
+        <label for="title">Title:</label>
+        <input type="text" class="form-control" id="title" name="title">
       </div>
 
       <div class="form-group">
-        <label for="body">Body</label>
+        <label for="body">Body:</label>
         <textarea id="body" name="body" class="form-control"></textarea>
       </div>
 
-      <button type="submit" class="btn btn-primary">Publish</button>
+      <div class="form-group">
+        <button type="submit" class="btn btn-primary">Publish</button>
+      </div>
+
+      @include('layouts.errors')
     </form>
+
   </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@
 
 Route::get('/', 'PostsController@index');
 Route::get('/posts/create', 'PostsController@create');
-// Route::get('/posts/{post}', 'PostsController@show');
+Route::post('/posts', 'PostsController@store');
+Route::get('/posts/{post}', 'PostsController@show');
