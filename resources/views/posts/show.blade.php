8f501abcf920003301574889aea359f4015206b6
patmolera <pat_molera@yahoo.com>
2017-08-14 21:28:00 +0800

    Adding a comment

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,5 +5,38 @@
     <h1>{{ $post->title }}</h1>
 
     {{ $post->body }}
+
+    <div class="comments">
+      <ul class="list-group">
+
+        @foreach ($post->comments as $comment)
+          <li class="list-group-item">
+            <strong>
+              {{ $comment->created_at->diffForHumans() }}: &nbsp;
+            </strong>
+
+            {{ $comment->body }}
+          </li>
+        @endforeach
+      </ul>
+    </div>
+
+    {{-- Add Comment --}}
+    <hr>
+    <div class="card">
+      <div class="card-block">
+        <form class=""method="POST" action="/posts/{{ $post->id}}/comments">
+          {{ csrf_field() }}
+          <div class="form-group">
+            <textarea name="body" rows="3" placeholder="Your Comment Here." class="form-control"></textarea>
+          </div>
+
+          <div class="form-group">
+            <button type="submit" class="btn btn-primary">Add Comment</button>
+          </div>
+        </form>
+      </div>
+    </div>
+
   </div>
 @endsection
