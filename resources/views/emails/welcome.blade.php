24ceff3ac08df21fc679c754bd18529c254e246f
patmolera <pat_molera@yahoo.com>
2017-10-30 05:15:08 +0800

    Sending Mail

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Email</title>
+  </head>
+  <body>
+    <h1>Welcome to Patrick Blog, {{ $user->name }}</h1>
+  </body>
+</html>
