d3df5c7f8cae7fb98cd1bdc72ff1dc3f1b898d52
patmolera <pat_molera@yahoo.com>
2017-10-30 15:04:43 +0800

    Request and Flash Messaging

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\User;
-use App\Mail\Welcome;
+use App\Http\Requests\RegistrationRequest;
 
 class RegistrationController extends Controller
 {
@@ -13,19 +11,11 @@ class RegistrationController extends Controller
       return view('registration.create');
     }
 
-    public function store()
+    public function store(RegistrationRequest $form)
     {
-      $this->validate(request(), [
-        'name' => 'required',
-        'email' => 'required|email',
-        'password' => 'required|confirmed'
-      ]);
+      $form->persist();
 
-      $user = User::create(request(['name', 'email', 'password']));
-
-      auth()->login($user);
-
-      \Mail::to($user)->send(new Welcome($user));
+      session()->flash('message', 'Thanks for Signing Up.');
 
       return redirect()->home();
     }
